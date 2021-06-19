0b0f1620dae5f1635e7e61701f896860f2cca4f8
mekagazira <soda_pop1985@yahoo.co.jp>
2021-06-19 20:15:14 +0900

    東京都の人口を年毎にグラフ化

--- main3.py
+++ main3.py
@@ -8,22 +8,21 @@ font = {'family': 'meiryo'}
 matplotlib.rc('font', **font)
 
 df_population_data = pd .read_csv("data.csv", encoding="shift-jis")
-df_population_data_mean = df_population_data.groupby(by="都道府県名").mean()[["人口（総数）", "人口（男）", "人口（女）"]].round(1)
-df_population_data_mean_sort =df_population_data_mean.sort_values(by="人口（総数）", ascending=False)
+df_population_data_tokyo = df_population_data[df_population_data['都道府県名']=='東京都'][["西暦（年）", "人口（総数）", "人口（男）", "人口（女）"]]
 
 
 # 図の幅と高さをインチで指定する。※figsize パラメータのデフォルト値は [6.4、4.8]らしい。
 plt.rcParams["figure.figsize"] = (9, 6)
 
-# グラフを描く
-df_population_data_mean_sort.plot()
+# グラフを描く（x軸, y軸としてプロットする列を選択する）
+df_population_data_tokyo.plot(x='西暦（年）', y=["人口（総数）", "人口（男）", "人口（女）"])
 
-plt.xlabel("x-lable：都道府県名", color="blue")
+plt.xlabel("x-lable：西暦（年）", color="blue")
 plt.ylabel("y-lable：人口")
 
 # 軸の最大値・最小値の設定
-plt.ylim(0, 10000000)
-plt.yticks(np.arange(0, 10000001, step=1000000))
+plt.ylim(0, 15000000)
+plt.yticks(np.arange(0, 15000001, step=1000000))
 
 # 指数表記から普通の表記に変換
 plt.ticklabel_format(style='plain', axis='y')
